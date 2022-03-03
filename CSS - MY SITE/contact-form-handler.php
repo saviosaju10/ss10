ca4a66ec806c7eb5bd0e6fcb4246701e745127d4
saviosaju10 <100836304+saviosaju10@users.noreply.github.com>
2022-03-03 15:27:13 +0530

    Add files via upload
    add the initial files needed.

---
+++ CSS - MY SITE/contact-form-handler.php
@@ -0,0 +1,37 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $POST['message'];
+
+
+
+
+    $email_from = $visitor_email;
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                   "User Email: $visitor_email.\n".
+                   "User Message: $message.\n".
+
+    $to = "saviosaju10@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers = "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index2.html");
+
+
+
+
+
+
+
+
+
+
+
+?>
